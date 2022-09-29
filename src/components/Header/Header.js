ff2882ee94ccb9c7cc3926ec3ed5c9f18b32bf29
Tamannarahman <tamannarahmanmridha@gmail.com>
2022-09-29 17:20:49 +0600

    header component added

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div>
+            <h1 className='header'>Learn Activity</h1>
+        </div>
+    );
+};
+
+export default Header;
